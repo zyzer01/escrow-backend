92553884d11ff8b2217c51dfaed297a8bf2db4d9
David Onifade <davidicfola@gmail.com>
2024-09-24 20:25:39 +0100

    remove: notfound and custom error middlewares

--- src/lib/middleware/errors.ts
+++ src/lib/middleware/errors.ts
@@ -26,8 +26,8 @@ export function errorHandler(
 }
 
 // Middleware to catch 404 errors
-export function notFoundHandler(req: Request, res: Response, next: NextFunction) {
-  const error = new Error(`Not Found - ${req.originalUrl}`);
-  res.status(404);
-  next(error);
-}
+// export function notFoundHandler(req: Request, res: Response, next: NextFunction) {
+//   const error = new Error(`Not Found - ${req.originalUrl}`);
+//   res.status(404);
+//   next(error);
+// }
