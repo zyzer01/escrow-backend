4d6e21d28c972a8e38e962d92698ff9678410d44
David Onifade <davidicfola@gmail.com>
2025-02-10 20:27:54 +0100

    update inversify.config.ts and bet.service.ts

--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -1,11 +1,13 @@
 import { Container } from 'inversify';
 import { BetService } from './resources/bets/bet.service';
 import { RejectBetInvitationProvider } from './resources/bets/providers/reject-bet-invitation.provider';
+import { CreateBetProvider } from './resources/bets/providers/create-bet.provider';
 
 const container = new Container();
 
 // Bind providers
 container.bind<RejectBetInvitationProvider>(RejectBetInvitationProvider).toSelf();
+container.bind<CreateBetProvider>(CreateBetProvider).toSelf();
 
 // Bind services
 container.bind<BetService>(BetService).toSelf();
